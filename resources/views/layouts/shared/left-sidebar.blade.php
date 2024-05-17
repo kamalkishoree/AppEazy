9590ed5f0028cf1d31c965997b04bd5a2d21815c
Anuj Sharma <anuj.sharma@codebrewinnovations.com>
2024-05-17 19:06:58 +0530

    conflict fix

--- resources/views/layouts/shared/left-sidebar.blade.php
+++ resources/views/layouts/shared/left-sidebar.blade.php
@@ -2,16 +2,7 @@
     $getAdditionalPreference = getAdditionalPreference(['is_seller_module','is_gift_card','is_marg_enable','is_vendor_marg_configuration','is_car_rental_enable']);
 @endphp
 <div class="left-side-menu">
-<<<<<<< HEAD
-<<<<<<< HEAD
     <div class="logo-box m-hide d-lg-block" style = "height : auto" > 
-=======
-    <div class="logo-box   d-lg-block" style="height: auto">
->>>>>>> super_easy_anuj
-=======
-    <div class="logo-box m-hide d-lg-block" style="height: auto">
-
->>>>>>> deevanshi/order_panel_issues
         @php
             $urlImg = URL::to('/').'/assets/images/users/user-1.jpg';
             $clientData = \App\Models\Client::select('id', 'logo','dark_logo','socket_url')->first();
