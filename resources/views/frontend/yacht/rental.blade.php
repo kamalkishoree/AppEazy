49b49616cff34d878ee9a937a1f7c359b9d4cc12
Ubuntu <ubuntu@ip-172-31-8-52.me-south-1.compute.internal>
2024-08-22 12:25:27 +0000

    car-rental pickup date

--- resources/views/frontend/yacht/rental.blade.php
+++ resources/views/frontend/yacht/rental.blade.php
@@ -143,15 +143,7 @@
             </div>
         </div>
     </div>
-    {{--@include('layouts.store.remove_cart_model')
-     @include('layouts.store.car-rental-footer') --}}
-
+     @include('layouts.store.remove_cart_model')
+     @include('layouts.store.car-rental-footer') 
+    <script src="{{ asset('js/car-rental.js') }}"></script>
 </section>
-
-@section('script')
-<script type="text/javascript"src="{{asset('front-assets/js/slick.js')}}"></script>
-<script src="{{ asset('js/car-rental.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-<script src="{{asset('assets/libs/select2/select2.min.js')}}"></script>
-<script defer type="text/javascript" src="{{asset('front-assets/js/script.js')}}"></script>
-@endSection
